14bd7c97be1de98da7fc26df78709d35846ab64f
Jensooon <jenson.wintle@gmail.com>
2024-07-24 20:07:13 +0100

    Basic post request

--- src/app/computer-vision.service.ts
+++ src/app/computer-vision.service.ts
@@ -6,25 +6,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class ComputerVisionService {
-  private endpoint = 'https://test.cognitiveservices.azure.com/';
-  private subscriptionKey = '<my_subscription_key>';
-
-  constructor(private http: HttpClient) {}
-
-  analyzeImage(imageUrl: string): Observable<any> {
-    const headers = new HttpHeaders({
-      'Ocp-Apim-Subscription-Key': this.subscriptionKey,
-      'Content-Type': 'application/json',
-    });
-
-    const body = {
-      url: imageUrl,
-    };
-
-    return this.http.post(
-      `${this.endpoint}/vision/v3.1/analyze?visualFeatures=Categories,Description,Color`,
-      body,
-      { headers }
-    );
-  }
+  // private endpoint = 'https://test.cognitiveservices.azure.com/';
+  // private subscriptionKey = '<my_subscription_key>';
+  // constructor(private http: HttpClient) {}
+  // analyzeImage(imageUrl: string): Observable<any> {
+  //   const headers = new HttpHeaders({
+  //     'Ocp-Apim-Subscription-Key': this.subscriptionKey,
+  //     'Content-Type': 'application/json',
+  //   });
+  //   const body = {
+  //     url: imageUrl,
+  //   };
+  //   return this.http.post(
+  //     `${this.endpoint}/vision/v3.1/analyze?visualFeatures=Categories,Description,Color`,
+  //     body,
+  //     { headers }
+  //   );
+  // }
 }
--- src/app/computer-vision/computer-vision.component.ts
+++ src/app/computer-vision/computer-vision.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ComputerVisionService } from '../computer-vision.service.js';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-computer-vision',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './computer-vision.component.html',
   styleUrl: './computer-vision.component.scss',
 })
@@ -13,18 +14,26 @@ export class ComputerVisionComponent {
   title = 'azure-computer-vision';
   analysisResult: any;
 
-  constructor(private computerVisionService: ComputerVisionService) {}
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com/';
 
-  analyzeImage() {
-    const imageUrl = '/assets/Owl.jpg'; // Replace with the actual image URL
-    this.computerVisionService.analyzeImage(imageUrl).subscribe(
-      (data) => {
-        this.analysisResult = data;
-        console.log(data);
-      },
-      (error) => {
-        console.error('Error analyzing image:', error);
-      }
-    );
+  posts: any;
+
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    this.posts = this.http.get(this.ROOT_URL + '/posts');
   }
+
+  // analyzeImage() {
+  //   const imageUrl = '/assets/Owl.jpg'; // Replace with the actual image URL
+  //   this.computerVisionService.analyzeImage(imageUrl).subscribe(
+  //     (data) => {
+  //       this.analysisResult = data;
+  //       console.log(data);
+  //     },
+  //     (error) => {
+  //       console.error('Error analyzing image:', error);
+  //     }
+  //   );
+  // }
 }
