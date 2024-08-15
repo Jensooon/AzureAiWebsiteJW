651329c2354cda8b7c8a5114fd9e5ee99207dd48
Jensooon <jenson.wintle@gmail.com>
2024-08-15 09:44:03 +0100

    Implemented navigation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ComputerVisionComponent } from './computer-vision/computer-vision.component';
 
-export const routes: Routes = [];
+const routeConfig: Routes = [
+  { path: 'home', component: HomeComponent, title: 'Home' },
+  {
+    path: 'vision',
+    component: ComputerVisionComponent,
+    title: 'Computer Vision Capabilities',
+  },
+  //TODO: Make home default path once done testing with vision
+  { path: '', redirectTo: '/vision', pathMatch: 'full' },
+];
+
+export default routeConfig;
