0d29dea2f6a8c05960f2eb2b1337193d872abe62
Jensooon <jenson.wintle@gmail.com>
2024-08-24 20:48:36 +0100

    Implemented click events to dashboard.

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { title } from 'process';
 
 @Component({
   selector: 'app-dashboard',
@@ -31,23 +32,43 @@ export class DashboardComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
+          {
+            title: 'Vision',
+            cols: 1,
+            rows: 1,
+            onClick: () => this.handleVisionClick(),
+          },
+          {
+            title: 'Language',
+            cols: 1,
+            rows: 1,
+            onClick: () => this.handleLanguageClick(),
+          },
           { title: 'Card 3', cols: 1, rows: 1 },
           { title: 'Card 4', cols: 1, rows: 1 },
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 1, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
+        { title: 'Vision', cols: 1, rows: 1 },
+        { title: 'Language', cols: 1, rows: 1 },
         { title: 'Card 3', cols: 1, rows: 1 },
         { title: 'Card 4', cols: 1, rows: 1 },
       ];
     })
   );
 
-  test() {
-    console.log(this.cards);
+  handleVisionClick() {
+    console.log('Card 1 clicked');
+  }
+
+  handleLanguageClick() {
+    console.log('Card 2 clicked');
+  }
+
+  onCardClick(card: any): void {
+    if (card.onClick) {
+      card.onClick();
+    }
   }
 }
