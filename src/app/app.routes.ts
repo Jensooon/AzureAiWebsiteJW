cc0f697bb1777b41f8c5ff7816d186a8011fc20c
Jensooon <jenson.wintle@gmail.com>
2024-08-25 13:44:25 +0100

    Added speech component. Linked with navigation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ComputerVisionComponent } from './computer-vision/computer-vision.component';
 import { LanguageComponent } from './language/language.component';
-import { DashboardComponent } from './home/dashboard/dashboard.component';
+import { SpeechComponent } from './speech/speech.component';
 
 const routeConfig: Routes = [
   { path: 'home', component: HomeComponent, title: 'Home' },
@@ -15,6 +15,11 @@ const routeConfig: Routes = [
     path: 'language',
     component: LanguageComponent,
     title: 'Language Capabilities',
+  },
+  {
+    path: 'speech',
+    component: SpeechComponent,
+    title: 'Speech Capabilities',
   },
 
   //TODO: Make home default path once done testing with language
