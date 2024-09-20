fa84fbaf0341e42d48184efaa9a1b96f560c1eb4
Jensooon <jenson.wintle@gmail.com>
2024-09-20 23:29:51 +0100

    Added introduction

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatCard } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, MatCardModule, MatCard],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
